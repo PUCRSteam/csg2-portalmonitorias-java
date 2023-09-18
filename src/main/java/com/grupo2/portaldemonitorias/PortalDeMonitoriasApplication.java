9246f81906839f005a2b387f86a7284d9c481364
Marcos Sanhudo <marcos.m@edu.pucrs.br>
2023-09-17 21:07:25 -0300

    boilerplate

---
+++ src/main/java/com/grupo2/portaldemonitorias/PortalDeMonitoriasApplication.java
@@ -0,0 +1,13 @@
+package com.grupo2.portaldemonitorias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalDeMonitoriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalDeMonitoriasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo2/portaldemonitorias/PortalDeMonitoriasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo2.portaldemonitorias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalDeMonitoriasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
