d91f2d332357699062217dbdd7a9d0156a74da7c
Alexya Silva <alexya.oliveira@edu.pucrs.br>
2023-09-19 18:11:26 -0300

    Adding Professor Model and Enum

---
+++ src/main/java/com/grupo2/portaldemonitorias/Enum/Gender.java
@@ -0,0 +1,56 @@
+package com.grupo2.portaldemonitorias.Enum;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.annotation.JsonView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Gender {
+
+    M("M","Male"),
+    F("F","Female"),
+    O("O", "Others");
+
+    private final String chave;
+    private final String descricao;
+
+    private static final Map<String, Gender> byDescription = new HashMap<>();
+
+    static{
+        for(Gender g : values()){
+            byGender.put(g.chave, g);
+        }
+    }
+
+    Gender(String chave, String descricao) {
+        this.chave = chave;
+        this.descricao = descricao;
+    }
+
+    public String getChave() {
+        return chave;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @JsonValue
+    public String getValor() {
+        switch (this) {
+            case M:
+            case F:
+            case O:
+                return valueOfDescription(getChave()).getDescricao();
+        }
+        return "Valor Inválido";
+    }
+
+    @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
+    public static Gender valueOfDescription(String key){
+        return byGender.get(key);
+    }
+
+}
--- src/main/java/com/grupo2/portaldemonitorias/Model/Professor.java
+++ src/main/java/com/grupo2/portaldemonitorias/Model/Professor.java
@@ -1,5 +1,42 @@
+package com.grupo2.portaldemonitorias.Model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import com.grupo2.portaldemonitorias.Enum.Gender;
+
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+
+import javax.persistence.Table;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "professores", schema = "personas")
+
 public class Professor {
-    private int id;
-    private String name;
-    private String gender;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_professor")
+    private Long idProfessor;
+
+    @Lob
+    @Type(type = "org.hibernate.type.TextType")
+    @Column(name = "professor_name")
+    private String professorName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "gender")
+    private Gender gender;
+
+    public Professor() {}
+
+    public Long getId() {
+        return getIdProfessor();
+    }
+
 }
