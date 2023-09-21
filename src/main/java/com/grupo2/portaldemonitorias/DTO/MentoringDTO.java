b0fab501001273058f9bf2c34bce9551b8d59b47
Alexya Silva <alexya.oliveira@edu.pucrs.br>
2023-09-21 17:53:03 -0300

    Adding DTOS

---
+++ src/main/java/com/grupo2/portaldemonitorias/DTO/MentoringDTO.java
@@ -0,0 +1,42 @@
+package com.grupo2.portaldemonitorias.DTO;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+
+public class MentoringDTO {
+    // @ApiModelProperty(position = 1, notes = "Identificação única da monitoria")
+    private Long idMentoring;
+    // @ApiModelProperty(position = 2, notes = "Curso da monitoria")
+    private String course;
+
+    public MentoringDTO() {}
+
+    public Long getId(){
+        return getIdMentoring();
+    }
+
+/* 
+  == Retirar este bloco se não for necessário ==
+    public MentoringDTO(Long idMentoring, String course){
+        this.idMentoring = idMentoring;
+        this.course = course;
+    }
+    public Long getIdMentoring(){
+        return idMentoring;
+    }
+    public void setIdMentoring(Long idMentoring){
+        this.idMentoring = idMentoring;
+    }
+    public String getCourse(){
+        return course;
+    }
+    public void setCourse(String course){
+        this.course = course;
+    }
+*/
+}
---
+++ src/main/java/com/grupo2/portaldemonitorias/DTO/ProfessorDTO.java
@@ -0,0 +1,43 @@
+package com.grupo2.portaldemonitorias.DTO;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+
+public class ProfessorDTO {
+   // @ApiModelProperty(position = 1, notes = "Identificação única do professor")
+    private Long idProfessor;
+   // @ApiModelProperty(position = 2, notes = "Nome do professor")
+    private String professorName;
+
+    public ProfessorDTO() {}
+
+    public Long getId(){
+        return getIdProfessor();
+    }
+
+
+/* 
+== Retirar este bloco se não for necessário ==
+    public ProfessorDTO(Long idProfessor, String professorName){
+        this.idProfessor = idProfessor;
+        this.professorName = professorName;
+    }
+    public Long getIdProfessor(){
+        return idProfessor;
+    }
+    public void setIdProfessor(Long idProfessor){
+        this.idProfessor = idProfessor;
+    }
+    public String getProfessorName(){
+        return professorName;
+    }
+    public void setProfessorName(String professorName){
+        this.professorName = professorName;
+    }
+*/
+}
