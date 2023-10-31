e7ccdf1ceb58871b7cd3b8a0008648d0696d33d8
Adriana A. Serpa <68662079+Adriana-Serpa99@users.noreply.github.com>
2023-10-31 18:24:47 -0300

    fixing yet AGAIN

--- src/main/java/com/grupo2/portaldemonitorias/Service/MentoringService.java
+++ src/main/java/com/grupo2/portaldemonitorias/Service/MentoringService.java
@@ -7,10 +7,6 @@ import com.grupo2.portaldemonitorias.Repository.MentoringRepository;
 import com.grupo2.portaldemonitorias.Repository.ProfessorRepository;
 import com.grupo2.portaldemonitorias.Repository.StudentRepository;
 
-//Check These later to see if needed.
-import com.grupo2.portaldemonitorias.Service.StudentService;
-import com.grupo2.portaldemonitorias.Service.ProfessorService;
-
 @Service
 public class MentoringService {
 
@@ -28,14 +24,19 @@ public class MentoringService {
         @Autowired
         private MentoringRepository mentoringRepository;
 
+        //The next two are being problematic
+
         //service estudante
-        private StudentService studentService = new StudentService();
+        //private StudentService studentService = new StudentService();
+        studentService (new StudentService());
 
         //service professor
-        private ProfessorService professorService = new ProfessorService();
+        //private ProfessorService professorService = new ProfessorService();
+        professorService (new ProfessorService());
         
-        
-        Mentoring mentoring= MentoringMapper.MAPPER.mentoringDTO(mentoringDTO);
+        // ------------------------------------------------------
+
+        Mentoring mentoring = MentoringMapper.MAPPER.mentoringDTO(mentoringDTO);
 
         MentoringRepository.save(mentoring);
     }
